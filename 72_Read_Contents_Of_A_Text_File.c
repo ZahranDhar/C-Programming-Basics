4cd08e1dc5e85e201a7b450fbc178b216fbde5fe
Zahran Dhar <zahrannazirdhar@gmail.com>
2024-09-06 11:23:45 +0530

    Update 72_Read_Contents_Of_A_Text_File.c

--- 72_Read_Contents_Of_A_Text_File.c
+++ 72_Read_Contents_Of_A_Text_File.c
@@ -7,12 +7,22 @@ int main()
     char  text[100];
     FILE *F= fopen("read.txt","r");
 
-    while((fgets(text,100,F))!=NULL)
+    if(F==NULL)
     {
-        printf("%s",text);
+        printf("Unable to open the file.\n");
+    }
+    else
+    {
+        char name[10];
+        int age;
+
+        fscanf(F,"%s %d",&name,&age);
+
+        printf("Your name is %s and your age is %d.",name,age);
+    
+        fclose(F);
     }
     
-    fclose(F);
-    
+
     return 0;
 }
