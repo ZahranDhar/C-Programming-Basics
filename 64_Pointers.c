0d4d1b0bac33e071ffd6022e40cba3295efdf626
Zahran Dhar <zahrannazirdhar@gmail.com>
2024-09-08 20:35:47 +0530

    Update 64_Pointers.c

--- 64_Pointers.c
+++ 64_Pointers.c
@@ -1,20 +1,24 @@
 // Demonstrate use of pointers in programming.
 
 #include<stdio.h>
-
+void fage(int *);
 int main()
 {
-    int age,*pAge;
+    int age;
 
     printf("What is your age?\n");
     scanf("%d",&age);
 
-    pAge=&age;
-
     printf("The location of age is %p\n",&age);
-    printf("The location of pAge is %p\n",pAge);
     printf("The value of age is %d\n",age);
-    printf("The value of pAge is %d\n",*pAge);
+
+    fage(&age);
+    
 
     return 0;
+}
+void fage(int *pAge)
+{   
+    printf("The location of pAge is %p\n",pAge);
+    printf("The value of pAge is %d\n",*pAge);
 }
